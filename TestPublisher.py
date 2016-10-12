16d516dd0d99f13b8b4a2407765e9228c6694cdd
Victor Nielsen <victor.nielsen@hotmail.com>
2016-10-12 14:08:33 +0200

    Working Puclisher and Subscriber

--- TestPublisher.py
+++ TestPublisher.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
-
+# license removed for brevity
 import rospy
-import math
-import time
+from std_msgs.msg import String
 
-def VisionCoordinates():
-    pub = rospy.Publisher('Coordinates', list, queue_size=10)
-    rospy.init_node('VisionCoordinates', anonymous=True)
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        listen = [20,20]
-        pub.publish(listen)
+        
+        test_data = "%f,%f"%(20.3,11.3)       
+        rospy.loginfo(test_data)
+        pub.publish(test_data)
         rate.sleep()
 
 if __name__ == '__main__':
-    VisionCoordinates()
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
