a2f1526b1d7dfa57b65859d0e6377ebb810800b9
Rikke Tranborg <rikke_tranborg@hotmail.com>
2016-10-21 11:16:54 +0200

    Deleted comments and corrected color thresholds

--- findBricks.py
+++ findBricks.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
 import cv2
 import urllib
 import numpy as np
 import math
 
 def get_from_webcam():
-    """
-    Fetches an image from the webcam
-    """
     print "try fetch from webcam..."
     stream=urllib.urlopen('http://192.168.0.20/image/jpeg.cgi')
     bytes=''
@@ -25,11 +21,6 @@ def get_from_webcam():
         print "did not receive image, try increasing the buffer size in line 13:"
 
 def extract_single_color_range(image,hsv,lower,upper):
-    """
-    Calculates a mask for which all pixels within the specified range is set to 1
-    the ands this mask with the provided image such that color information is
-    still present, but only for the specified range
-    """
     if len(lower) == 2 and len(upper) == 2:
         mask0 = cv2.inRange(hsv, lower[0], upper[0])
         mask1 = cv2.inRange(hsv, lower[1], upper[1])
@@ -40,35 +31,17 @@ def extract_single_color_range(image,hsv,lower,upper):
     return res
 
 def threshold_image(image):
-    """
-    Thresholds the image within the desired range and then dilates with a 3x3 matrix
-    such that small holes are filled. Afterwards the 'blobs' are closed using a
-    combination of dilate and erode
-    """
     ret,th1 = cv2.threshold(image,50,255,cv2.THRESH_BINARY)
     resdi = cv2.dilate(th1,np.ones((3,3),np.uint8))
     closing = cv2.morphologyEx(resdi, cv2.MORPH_CLOSE,np.ones((5,5),np.uint8))
-    
     return closing
 
 def contours(image):
-    """
-    Extract the contours of the image by first converting it to grayscale and then
-    call findContours
-    """
     imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     contours, hierarchy = cv2.findContours(imgray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
     return contours
 
 def get_centers(contours):
-    """
-    For each contour in contours
-        approximate the contours such that small variations are removed
-        calulate the area of the contour
-        if the area is within the desired range we append the box points to the
-        bricks.
-    """
     centers = []
     for cnt in contours:
         epsilon = 0.1*cv2.arcLength(cnt,True)
@@ -80,27 +53,19 @@ def get_centers(contours):
             centers.append((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
     
     return centers
-    
 
-def find_brick_centers():
-    """
-    Main methods for processing an image and detect rectangles in the given
-    hsv color range
-
-    set debug to True in order to show the intermediate images
-    """
-    
+def find_brick_centers():    
     lower_blue = np.array([92,76,103])
     upper_blue = np.array([141,255,255])
     
-    lower_green = np.array([35,76,0])
+    lower_green = np.array([36,76,0])
     upper_green = np.array([74,255,255])
     
-    lower_yellow = np.array([20,76,103])
-    upper_yellow = np.array([53,255,255])
+    lower_yellow = np.array([21,76,103])
+    upper_yellow = np.array([38,255,255])
     
-    lower_red = np.array([np.array([0,76,103]),np.array([162,76,103])])
-    upper_red = np.array([np.array([14,255,255]),np.array([180,255,255])])
+    lower_red = np.array([np.array([0,76,103]),np.array([161,76,103])])
+    upper_red = np.array([np.array([14,255,255]),np.array([179,255,255])])
 
     image = get_from_webcam()
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
